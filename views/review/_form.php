5f4809cb90a5590b30b3658ac9901343d46c0a0b
PWNERCORE <2b7e151628aed2a6@gmail.com>
2021-07-31 19:50:14 +0400

    Created crud for review,city / by gii

---
+++ views/review/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Review */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="review-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'city_id')->textInput() ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'rating')->textInput() ?>
+
+    <?= $form->field($model, 'img')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'author_id')->textInput() ?>
+
+    <?= $form->field($model, 'creation_date')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
