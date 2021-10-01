21fb14d25427674b921145c15c1c5bbcdd0b91ab
PWNERCORE <2b7e151628aed2a6@gmail.com>
2021-10-01 16:19:38 +0400

    simply modified

--- components/GeolocationComponent.php
+++ components/GeolocationComponent.php
@@ -4,6 +4,7 @@ namespace app\components;
 
 use Dadata\DadataClient;
 use app\models\City;
+use Yii;
 use yii\base\Component;
 use yii\helpers\Html;
 
@@ -16,20 +17,18 @@ class GeolocationComponent extends Component {
         parent::init();
         return null;
     }
+    protected $token = "4ffe29813ad9f908905e230f3f1b9ea7f96a476b";
+    protected $secret = "ef2b8c507125f23231f71dfca6f42bfcef0e40a8";
 
     public function getCity() {
-        $token = "4ffe29813ad9f908905e230f3f1b9ea7f96a476b";
-        $secret = "ef2b8c507125f23231f71dfca6f42bfcef0e40a8";
-        $dadata = new DadataClient($token, $secret);
-        //$response = $api->iplocate(Yii::$app->getRequest()->getUserIP());
-        $response = $dadata->iplocate('46.226.227.20');
+        $dadata = new DadataClient($this->token, $this->secret);
+        $response = $dadata->iplocate(Yii::$app->getRequest()->getUserIP());
+        // $response = $dadata->iplocate('46.226.227.20'); used for debug
         return $response['value'];
     }
 
     public function inputCity($input) {
-        $token = "4ffe29813ad9f908905e230f3f1b9ea7f96a476b";
-        $secret = "ef2b8c507125f23231f71dfca6f42bfcef0e40a8";
-        $dadata = new DadataClient($token, $secret);
+        $dadata = new DadataClient($this->token, $this->secret);
         $result = $dadata->suggest("address", $input);
         if (is_integer($input)) {
             return $input;
